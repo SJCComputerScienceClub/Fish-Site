5d968c6ee4b3a81fb22a6128dc2ddcf54e3e5b19
grzelaksjc <93996198+grzelaksjc@users.noreply.github.com>
2025-10-16 08:45:16 -0400

    Refactor fish movement functions

--- swim.js
+++ swim.js
@@ -1,25 +1,3 @@
-function moveFish(fishElement) {
-  const aquarium = document.getElementById('aquarium');
-  const maxX = aquarium.clientWidth - fishElement.clientWidth;
-  const maxY = aquarium.clientHeight - fishElement.clientHeight;
-  const newX = Math.random() * maxX;
-  const newY = Math.random() * maxY;
-  const rotate = Math.random() < 0.5 ? '' : 'scaleX(-1)';
-
-  fishElement.style.transform = `translate(${newX}px, ${newY}px) ${rotate}`;
-}
-function animateFish() {
-  const fishes = document.querySelectorAll('.fish');
-  fishes.forEach(f => {
-    moveFish(f);
-  });
-  // repeat every few seconds
-  setTimeout(animateFish, 3000 + Math.random()*2000);
-}
-
-window.onload = () => {
-  animateFish();
-};
 function swimFish(fish) {
   const aquarium = document.getElementById('aquarium');
   const maxX = aquarium.clientWidth - fish.clientWidth;
@@ -76,6 +54,7 @@ window.onload = () => {
     swimFish(f);
   });
 };
+
 
 
 
