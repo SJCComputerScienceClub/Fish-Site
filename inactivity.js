0e03724eb672b78757d5bf6af8860b2851581062
grzelaksjc <93996198+grzelaksjc@users.noreply.github.com>
2026-03-12 13:24:33 -0400

    Add inactivity timer to redirect after timeout

---
+++ inactivity.js
@@ -0,0 +1,15 @@
+let inactivityTimer;
+const timeoutTime = 60000;
+
+function resetTimer() {
+    clearTimeout(inactivityTimer);
+    inactivityTimer = setTimeout(() => {
+        window.location.href = "index.html";
+    }, timeoutTime);
+}
+
+["mousemove","click","keydown","scroll","touchstart"].forEach(event => {
+    document.addEventListener(event, resetTimer);
+});
+
+resetTimer();
